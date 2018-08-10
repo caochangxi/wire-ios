691a90ff5b4ede790234a5a10a23b86123232273
Vytis Šibonis <vytis@wire.com>
2018-08-10 17:55:40 +0200

    fix font spec build failures

--- Wire-iOS Tests/FontSchemeTests.swift
+++ Wire-iOS Tests/FontSchemeTests.swift
@@ -30,7 +30,7 @@ class FontSchemeTests: XCTestCase {
                                                                          (fontSize: .medium, point: round(points[FontSize.medium]! * multiplier)),
                                                                          (fontSize: .small,  point: round(points[FontSize.small]! * multiplier))]
 
-        let allFontWeightTuples: [(fontWeight: FontWeight?, name: String)] = [(fontWeight: .ultraLight, name: "Ultralight"),
+        let allFontWeightTuples: [(fontWeight: FontWeight, name: String)] = [(fontWeight: .ultraLight, name: "Ultralight"),
                                                                               (fontWeight: .thin, name: "Thin"),
                                                                               (fontWeight: .light, name: "Light"),
                                                                               (fontWeight: .regular, name: "Regular"),
@@ -39,7 +39,6 @@ class FontSchemeTests: XCTestCase {
                                                                               (fontWeight: .bold, name: "Bold"),
                                                                               (fontWeight: .heavy, name: "Heavy"),
                                                                               (fontWeight: .black, name: "Black"),
-                                                                              (fontWeight: .none, name: "Light"),
                                                                               ]
 
 
@@ -75,26 +74,22 @@ class FontSchemeTests: XCTestCase {
 
         /// insert item for fontTextStyle = none
 
-        fixedFontNames[FontSpec(.large, .none)]       = "System-Light 24.0"
         fixedFontNames[FontSpec(.large, .medium)]     = "System-Medium 24.0"
         fixedFontNames[FontSpec(.large, .semibold)]   = "System-Semibold 24.0"
         fixedFontNames[FontSpec(.large, .regular)]    = "System-Regular 24.0"
         fixedFontNames[FontSpec(.large, .light)]      = "System-Light 24.0"
         fixedFontNames[FontSpec(.large, .thin)]       = "System-Thin 24.0"
         
-        fixedFontNames[FontSpec(.normal, .none)]      = "System-Light 16.0"
         fixedFontNames[FontSpec(.normal, .light)]     = "System-Light 16.0"
         fixedFontNames[FontSpec(.normal, .thin)]      = "System-Thin 16.0"
         fixedFontNames[FontSpec(.normal, .regular)]   = "System-Regular 16.0"
         fixedFontNames[FontSpec(.normal, .medium)]    = "System-Medium 16.0"
         fixedFontNames[FontSpec(.normal, .semibold)]  = "System-Semibold 16.0"
         
-        fixedFontNames[FontSpec(.medium, .none)]      = "System-Light 12.0"
         fixedFontNames[FontSpec(.medium, .medium)]    = "System-Medium 12.0"
         fixedFontNames[FontSpec(.medium, .semibold)]  = "System-Semibold 12.0"
         fixedFontNames[FontSpec(.medium, .regular)]   = "System-Regular 12.0"
         
-        fixedFontNames[FontSpec(.small, .none)]       = "System-Light 11.0"
         fixedFontNames[FontSpec(.small, .medium)]     = "System-Medium 11.0"
         fixedFontNames[FontSpec(.small, .semibold)]   = "System-Semibold 11.0"
         fixedFontNames[FontSpec(.small, .regular)]    = "System-Regular 11.0"
@@ -119,26 +114,22 @@ class FontSchemeTests: XCTestCase {
     func testThatItConvertsFontToClassyNameIfFontAdjustedExtraExtraExtraLarge() {
         var fixedFontNames: [FontSpec: String] = [:]
         
-        fixedFontNames[FontSpec(.large, .none)]      = "System-Light 33.0"
         fixedFontNames[FontSpec(.large, .medium)]    = "System-Medium 33.0"
         fixedFontNames[FontSpec(.large, .semibold)]  = "System-Semibold 33.0"
         fixedFontNames[FontSpec(.large, .regular)]   = "System-Regular 33.0"
         fixedFontNames[FontSpec(.large, .light)]     = "System-Light 33.0"
         fixedFontNames[FontSpec(.large, .thin)]      = "System-Thin 33.0"
         
-        fixedFontNames[FontSpec(.normal, .none)]     = "System-Light 22.0"
         fixedFontNames[FontSpec(.normal, .light)]    = "System-Light 22.0"
         fixedFontNames[FontSpec(.normal, .thin)]     = "System-Thin 22.0"
         fixedFontNames[FontSpec(.normal, .regular)]  = "System-Regular 22.0"
         fixedFontNames[FontSpec(.normal, .medium)]   = "System-Medium 22.0"
         fixedFontNames[FontSpec(.normal, .semibold)] = "System-Semibold 22.0"
 
-        fixedFontNames[FontSpec(.medium, .none)]     = "System-Light 17.0"
         fixedFontNames[FontSpec(.medium, .medium)]   = "System-Medium 17.0"
         fixedFontNames[FontSpec(.medium, .semibold)] = "System-Semibold 17.0"
         fixedFontNames[FontSpec(.medium, .regular)]  = "System-Regular 17.0"
         
-        fixedFontNames[FontSpec(.small, .none)]      = "System-Light 15.0"
         fixedFontNames[FontSpec(.small, .medium)]    = "System-Medium 15.0"
         fixedFontNames[FontSpec(.small, .semibold)]  = "System-Semibold 15.0"
         fixedFontNames[FontSpec(.small, .regular)]   = "System-Regular 15.0"
@@ -163,26 +154,22 @@ class FontSchemeTests: XCTestCase {
     func testThatItConvertsFontToClassyNameIfFontAdjustedExtraSmall() {
         var fixedFontNames: [FontSpec: String] = [:]
         
-        fixedFontNames[FontSpec(.large, .none)]       = "System-Light 20.0"
         fixedFontNames[FontSpec(.large, .medium)]     = "System-Medium 20.0"
         fixedFontNames[FontSpec(.large, .semibold)]   = "System-Semibold 20.0"
         fixedFontNames[FontSpec(.large, .regular)]    = "System-Regular 20.0"
         fixedFontNames[FontSpec(.large, .light)]      = "System-Light 20.0"
         fixedFontNames[FontSpec(.large, .thin)]       = "System-Thin 20.0"
         
-        fixedFontNames[FontSpec(.normal, .none)]      = "System-Light 13.0"
         fixedFontNames[FontSpec(.normal, .light)]     = "System-Light 13.0"
         fixedFontNames[FontSpec(.normal, .thin)]      = "System-Thin 13.0"
         fixedFontNames[FontSpec(.normal, .regular)]   = "System-Regular 13.0"
         fixedFontNames[FontSpec(.normal, .medium)]    = "System-Medium 13.0"
         fixedFontNames[FontSpec(.normal, .semibold)]  = "System-Semibold 13.0"
         
-        fixedFontNames[FontSpec(.medium, .none)]      = "System-Light 10.0"
         fixedFontNames[FontSpec(.medium, .medium)]    = "System-Medium 10.0"
         fixedFontNames[FontSpec(.medium, .semibold)]  = "System-Semibold 10.0"
         fixedFontNames[FontSpec(.medium, .regular)]   = "System-Regular 10.0"
         
-        fixedFontNames[FontSpec(.small, .none)]       = "System-Light 9.0"
         fixedFontNames[FontSpec(.small, .medium)]     = "System-Medium 9.0"
         fixedFontNames[FontSpec(.small, .semibold)]   = "System-Semibold 9.0"
         fixedFontNames[FontSpec(.small, .regular)]    = "System-Regular 9.0"
